9d34b73e04bf0724b97e4f390665355cd0a875f8
DaniGeraldine <gera08249091@gmail.com>
2015-08-21 01:12:11 -0500

    Update conten_usuario.php

--- application/views/conten_usuario.php
+++ application/views/conten_usuario.php
@@ -4,20 +4,18 @@
 ?>
 
 <div class="row">
-  <div class="col-md-3 col-md-offset-2">
-    <h2>Usuario</h2>
+  <div class="col-md-6">
+    <center><h2>Usuario</h2></center>
     <!--hay que cambiar la ruta de abajo el http-->
-    <a href="http://localhost/Layout/index.php/vistas/u">                  
-    <img src="<?php echo $rutaBase.'resources/imagenes/cinqueterre.jpg';?>" class="img-responsive" alt="Cinque Terre" width="204" height="136">
+    <a href="<?php echo $rutaBase.'index.php/vistas/page/u';?>">                 
+   <center><img src="<?php echo $rutaBase.'resources/imagenes/cinqueterre.jpg';?>" class="img-responsive" alt="Cinque Terre" width="204" height="136"></center>
   </div>
-</div>
 
-<div class="row">
-  <div class="col-xs-3 col-md-offset-2">
-    <h2>Graficas</h2>   
+  <div class="col-md-6">
+    <center><h2>Graficas</h2></center>
     <!--hay que cambiar la ruta de abajo el http-->
-    <a href="http://localhost/Layout/index.php/vistas/g"> 
-    <img src="<?php echo $rutaBase.'/resources/imagenes/grafica.jpg';?>" class="img-responsive" alt="Cinque Terre" width="304" height="236"> 
+    <a href="<?php echo $rutaBase.'index.php/vistas/page/g';?>">
+    <center><img src="<?php echo $rutaBase.'/resources/imagenes/grafica.jpg';?>" class="img-responsive" alt="Cinque Terre" width="304" height="236"></center> 
   </div>
 </div>
 
